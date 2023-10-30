fdff2ff63561e2a6ae6c12944bf9299ab0267eca
MickeyMemeDev <148511289+MickeyMemeDev@users.noreply.github.com>
2023-10-30 01:02:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+window.addEventListener('load', function() {
+  let hamburgers = document.getElementsByClassName("hamburger");
+  for (let i = 0; i < hamburgers.length; i++) {
+      hamburgers[i].addEventListener('click', function(event) {
+          let lists = document.getElementsByClassName("mobile_ul");
+          for (let j = 0; j < lists.length; j++) {
+              if (lists[j].classList.contains('visible')) {
+                  lists[j].classList.remove('visible');
+                  lists[j].classList.add('invisible');
+              } else {
+                  lists[j].classList.remove('invisible');
+                  lists[j].classList.add('visible');
+              }
+          }
+      });
+  }
+});
+
 function changeNav() {
   var x = document.getElementById("NavLinksRoot");
   if (x.className === "nav-links-root") {
